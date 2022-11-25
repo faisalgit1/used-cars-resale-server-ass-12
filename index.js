f4970f4683bba619b775bb04dfd818ca7d0a0073
faisalgit1 <mdfaisalahmed520@gmail.com>
2022-11-25 15:43:08 +0600

    connect-mongodb-database

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5bfvhe8.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 app.get('/', (req, res) => {
