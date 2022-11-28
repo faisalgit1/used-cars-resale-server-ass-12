3edd643fe295fd63e46025eb547546cad97c6599
faisalgit1 <mdfaisalahmed520@gmail.com>
2022-11-28 15:16:18 +0600

    jwt-implement

--- index.js
+++ index.js
@@ -18,11 +18,67 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
+        const usersCollection = client.db('resaleProducts').collection('users')
         app.post('/jwt', (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
             console.log(token);
             res.send({ token })
+        })
+
+        app.put("/user/:email", async (req, res) => {
+            try {
+                const email = req.params.email;
+
+                // check the req
+                const query = { email: email }
+                const existingUser = await usersCollection.findOne(query)
+
+                if (existingUser) {
+                    const token = jwt.sign(
+                        { email: email },
+                        process.env.ACCESS_TOKEN_SECRET,
+                        { expiresIn: "1d" }
+                    )
+                    return res.send({ data: token })
+                }
+
+                else {
+
+                    const user = req.body;
+                    const filter = { email: email };
+                    const options = { upsert: true };
+                    const updateDoc = {
+                        $set: user
+                    }
+                    const result = await usersCollection.updateOne(filter, updateDoc, options);
+
+                    // token generate 
+                    const token = jwt.sign(
+                        { email: email },
+                        process.env.ACCESS_TOKEN_SECRET,
+                        { expiresIn: "1d" }
+                    )
+                    return res.send({ data: token })
+
+                }
+
+            }
+            catch (err) {
+                console.log(err)
+            }
+        })
+        app.get('/users', async (req, res) => {
+
+            let query = {}
+            const result = await usersCollection.find(query).toArray();
+            res.send(result)
+        })
+        app.get('/chose-photos', async (req, res) => {
+            const query = {}
+            const cursor = usersCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
         })
     }
     finally {
